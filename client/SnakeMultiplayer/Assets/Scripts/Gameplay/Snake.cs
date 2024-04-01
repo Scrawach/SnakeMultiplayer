c3babeb47b36521024c32489c32f7a2fc6b128db
scrawach <scrawach@gmail.com>
2024-04-01 21:58:27 +0300

    added test tail component

--- client/SnakeMultiplayer/Assets/Scripts/Gameplay/Snake.cs
+++ client/SnakeMultiplayer/Assets/Scripts/Gameplay/Snake.cs
@@ -1,3 +1,4 @@
+using System;
 using Reflex.Attributes;
 using Services;
 using UnityEngine;
---
+++ client/SnakeMultiplayer/Assets/Scripts/Gameplay/Tail.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class Tail : MonoBehaviour
+    {
+        [SerializeField] private float _detailDistance = 1;
+        
+        private readonly Stack<Vector3> _history;
+
+        private void Start() => 
+            _history.Push(transform.position);
+
+        private void Update()
+        {
+            var previousPosition = _history.Peek();
+            var distance = Vector3.Distance(transform.position, previousPosition);
+
+            while (distance > _detailDistance)
+            {
+                var previous = _history.Pop();
+                var direction = (previous - transform.position).normalized;
+                _history.Push(previous + direction * _detailDistance);
+                distance -= _detailDistance;
+            }
+        }
+    }
+}
