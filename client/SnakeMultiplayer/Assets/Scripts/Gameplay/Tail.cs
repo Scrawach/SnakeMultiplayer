490f240ba344d3b5031fba4648f8cef08a644c81
scrawach <scrawach@gmail.com>
2024-04-02 13:44:58 +0300

    implemented base snake movement

--- client/SnakeMultiplayer/Assets/Scripts/Gameplay/Tail.cs
+++ client/SnakeMultiplayer/Assets/Scripts/Gameplay/Tail.cs
@@ -1,28 +1,41 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Gameplay
 {
     public class Tail : MonoBehaviour
     {
+        [SerializeField] private Transform _head;
+        [SerializeField] private List<Transform> _details;
         [SerializeField] private float _detailDistance = 1;
-        
-        private readonly Stack<Vector3> _history;
 
-        private void Start() => 
-            _history.Push(transform.position);
+        private List<Vector3> _history = new List<Vector3>();
+
+        private void Awake()
+        {
+            _history.Add(_head.position);
+            foreach (var detail in _details) 
+                _history.Add(detail.position);
+        }
 
         private void Update()
         {
-            var previousPosition = _history.Peek();
-            var distance = Vector3.Distance(transform.position, previousPosition);
-
+            var distance = Vector3.Distance(_head.position, _history[0]);
             while (distance > _detailDistance)
             {
-                var previous = _history.Pop();
-                var direction = (previous - transform.position).normalized;
-                _history.Push(previous + direction * _detailDistance);
+                var direction = (_head.position - _history[0]).normalized;
+                _history.Insert(0, _history[0] + direction * _detailDistance);
+                _history.RemoveAt(_history.Count - 1);
                 distance -= _detailDistance;
+            }
+
+            for (var i = 0; i < _details.Count; i++)
+            {
+                _details[i].position = Vector3.Lerp(_history[i + 1], _history[i], distance / _detailDistance);
+                var direction = (_history[i] - _history[i + 1]).normalized;
+                _details[i].position += direction * Time.deltaTime;
             }
         }
     }
